384e51eb3492cd2fcdc14c787b930adda20ad035
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2017-06-25 19:38:00 +0530

    videos data model added instead of running raw queries

--- main/models.py
+++ main/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Videos(models.Model):
+    course_name = models.CharField(max_length=256)
+    course_id = models.IntegerField()
+    video_link = models.URLField()
+    video_title = models.CharField(max_length=2048)
+    inserted_datetime = models.DateTimeField(auto_now_add=True)
